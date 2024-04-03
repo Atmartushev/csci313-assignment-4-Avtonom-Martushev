73f0442ec3f69844cb0f3e8613f19293e7ee2026
Atmartushev <atmartushev@gmail.com>
2024-04-03 18:30:59 -0500

    feat: created login/logout adn authentication

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
 from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
-
 from django.db.models import UniqueConstraint # Constrains fields to unique values
 from django.db.models.functions import Lower # Returns lower cased value of field
-
 import uuid
+from django.conf import settings
+from datetime import date
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -102,12 +101,21 @@ class BookInstance(models.Model):
         help_text='Book availability',
     )
 
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, 
+                                 null=True, blank=True)
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
     class Meta:
         ordering = ['due_back']
 
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+    
+
     
 class Author(models.Model):
     """Model representing an author."""
