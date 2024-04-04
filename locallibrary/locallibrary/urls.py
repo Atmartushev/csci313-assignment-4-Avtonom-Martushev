f40282d07838120fefead778a7eb700fb394f9e1
Atmartushev <atmartushev@gmail.com>
2024-04-03 19:46:52 -0500

    fix: broken logout

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -2,6 +2,7 @@ from django.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 """
 URL configuration for locallibrary project.
 
